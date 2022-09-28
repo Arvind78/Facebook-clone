d86cd0baf977909a4cc4efa8d4bbd1e09ebd295d
Arvind Varma <av894883@gmail.com>
2022-09-29 01:58:22 +0530

    jj

--- src/componet/Feed.js
+++ src/componet/Feed.js
@@ -8,14 +8,7 @@ const Feed = () => {
     <div className='feed'>
    <Storyreel/>
   <MassageSender/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Arvind kumar' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Rohit varma ' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName=' Mohit arya' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Sameer ' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Tanmay Nagar ' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Adesh more' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Sachin ' time="12:05pm"  massage=' this is a tempary massage'/>
-  <Post   photourl="https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg" image='https://i.postimg.cc/pXFJbx36/photo-1552642986-ccb41e7059e7-ixlib-rb-1-2.jpg' userName='Vishnu' time="12:05pm"  massage=' this is a tempary massage' /> 
+  <Post /> 
     </div>
   )
 }
--- src/componet/Post.js
+++ src/componet/Post.js
@@ -5,34 +5,48 @@ import { Avatar } from "@material-ui/core";
 import PublicIcon from '@material-ui/icons/Public';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-
+import {Data} from "./Data"
 import ShareIcon from '@material-ui/icons/Share';
 const Post = ({photourl,time,massage,userName,image}) => {
   return (
     <div className='post'>
+    {
+        Data.map((val)=>(
+          <>
     <div className="toppost">
+  
         <div className="toppostLeft">
-       <Avatar  src={photourl}/>
+    
+       <Avatar  src={val.avatar}/>
          <div className="postinfo">
-            <h4>{userName}</h4>
-            <p> {time} <PublicIcon/> </p>
+            <h4>{val.first_name}</h4>
+            <p>12:20 pm  <PublicIcon/></p>
          </div>
         </div>
       <MoreHorizIcon style={{ cursor: "pointer"}}/>
     </div>
     <div className="postmiddle">
-      <p> {massage}</p>
+      <p> {val.Post}</p>
 
-     {image && <img src={image} alt="Errar" />}
+     { <img src={val.postImg } alt="Errar" />}
 
      </div>
     <div className="post_bottom">
       <div className="post_bottom_option">
+        <div className="like">
     <ThumbUpIcon/><p>Like</p>
-    <ChatBubbleOutlineIcon/><p>Comment</p>
+    </div>
+    <div className="like">
+    <ChatBubbleOutlineIcon/><p>{val.comment} Comment</p>
+    </div>
+    <div className="like">
     <ShareIcon/><p>Share</p>
     </div>
     </div>
+   
+    </div>
+    </>
+  ))}
     </div>
   )
 }
